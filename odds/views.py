bc49b966a608b885fcce19842a3f2f0b603075f7
articulatestudio <lewis@articulate-studio.com>
2017-05-08 23:14:15 +0100

    Revert "remove race time filter"
    This reverts commit 3f94f89821d6c4902894757adbf3d5e937eba01e.

--- odds/views.py
+++ odds/views.py
@@ -343,7 +343,16 @@ def showTimes(data):
     current_time = get_current_time()
     courses = on_courses(current_time=current_time)
     print('courses: {}'.format(courses))
-    
+    try:
+        track_id = filter(lambda item: item['name'] == track and str(item['date']) >= current_time['iso_date'], tournaments_data)[0]['tournament_id']
+        print ('track_id: {}'.format(track_id))
+    except Exception as e:
+        print ('Python 3 exception: {}'.format(e))
+        try:
+            track_id = next(filter(lambda item: item['name'] == track and str(item['date']) >= current_time['iso_date'], tournaments_data))['tournament_id']
+        except Exception as e:
+            print('Exception: {}'.format(e))
+            track_id = ''
 
     print('trackid: {}'.format(track_id))
 
