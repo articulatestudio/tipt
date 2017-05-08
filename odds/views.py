4cfb1f4fc6136dfe185f4ac30051eeadfcdf6c47
articulatestudio <lewis@articulate-studio.com>
2017-05-08 17:23:53 +0100

    playing with time 3

--- odds/views.py
+++ odds/views.py
@@ -106,10 +106,10 @@ def get_current_time():
     """
     Get Current time
     """
-    print((datetime.now(timezone.utc) + timedelta(hours=2)).strftime("%Y%m%d%H%M"))
-    iso_date = (datetime.now(timezone.utc) + timedelta(hours=2)).isoformat()
+    print((datetime.now(timezone.utc) + timedelta(hours=-22)).strftime("%Y%m%d%H%M"))
+    iso_date = (datetime.now(timezone.utc) + timedelta(hours=-22)).isoformat()
     print('iso_date: {}'.format(iso_date))
-    date = ((datetime.now(timezone.utc) + timedelta(hours=2)).strftime("%Y%m%d%H%M"))
+    date = ((datetime.now(timezone.utc) + timedelta(hours=-22)).strftime("%Y%m%d%H%M"))
     year = date[:4]
     month_day = date[4:8]
     hour = date[8:10]
