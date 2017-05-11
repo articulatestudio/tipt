1c656df802820eb59ed3a727d7ba3516a8f204c0
articulatestudio <lewis@articulate-studio.com>
2017-05-11 17:19:51 +0100

    Revert "not equal to"
    This reverts commit e122cbbd07cbce25a0fb79fa0fdac368958e9067.

--- odds/views.py
+++ odds/views.py
@@ -215,7 +215,7 @@ def on_courses(current_time):
     """
     courses = []
     for item in tournaments_data:
-        if str(item['date']) =! str(current_time['iso_date']):
+        if str(item['date']) <= str(current_time['iso_date']):
             continue
         elif item['name'] not in courses:
             courses.append(item['name'])
