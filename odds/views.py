a636e6906801f02a888664ac6235b1c3e24bc365
articulatestudio <lewis@articulate-studio.com>
2017-05-17 11:31:52 +0100

    attempt to add emoji to races

--- odds/views.py
+++ odds/views.py
@@ -379,7 +379,7 @@ def showTimes(data):
 
         if len(replies) > 0:
             final_data = apiai_facebook_quick_reply(
-                "These are the next races at " + track + ". Let me know which race you want info for.",
+                "These are the next races at 📍" + track + ". Let me know which race you want info for.",
                 quick_replies=replies)
 
             return final_data
